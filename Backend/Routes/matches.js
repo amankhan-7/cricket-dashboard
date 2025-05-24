c7b9afc9e6011e5a3397edcab45192081629e423
amankhan-7 <139583696+amankhan-7@users.noreply.github.com>
2025-05-24 12:27:00 +0530

    import fix

--- Backend/Routes/matches.js
+++ Backend/Routes/matches.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Match = require('../models/match');
+const Match = require('../Models/match');
 
 router.get('/matches', async (req, res) => {
   try {
