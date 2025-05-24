32b54c46558c0e3118559f9e768970fcaf3a291c
amankhan-7 <139583696+amankhan-7@users.noreply.github.com>
2025-05-24 12:22:06 +0530

    first commit

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const matchRoutes = require('./routes/matches');
+const matchRoutes = require('./Routes/matches');
 
 dotenv.config();
 const app = express();
